a5d89ca5c30d8fcde291d2e6a51a815f5bc1f555
skuldomg <scdehne@googlemail.com>
2018-10-24 17:44:39 +0200

    First commit

---
+++ Pokemod/ModEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace Pokemod
+{
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        public override void Entry(IModHelper helper)
+        {
+            TimeEvents.AfterDayStarted += this.TimeEvents_AfterDayStarted;
+        }
+
+        /*********
+        ** Private methods
+        *********/
+        private void TimeEvents_AfterDayStarted(object sender, EventArgs e)
+        {
+            if(!Context.IsWorldReady)
+                return;
+                        
+            this.Monitor.Log("Welcome to Pokémod!");            
+
+            
+        }
+    }
+}
